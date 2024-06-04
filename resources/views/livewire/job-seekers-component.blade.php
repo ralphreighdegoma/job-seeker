3b982771cde388b87d8ac89530784246824ae2eb
wreck it ralph <personal.ralphdegoma@gmail.com>
2024-06-04 10:36:36 +0930

    first commit

--- resources/views/livewire/job-seekers-component.blade.php
+++ resources/views/livewire/job-seekers-component.blade.php
@@ -3,9 +3,6 @@
         <div class="absolute inset-0 bg-cover bg-center"></div>
         <div class="container mx-auto px-4 relative z-10">
             <!-- Big Heading -->
-            <h1 class="text-4xl md:text-5xl font-bold mb-30 text-center text-black my-20">
-                Browse Our Curated Selection of Virtual Assistants
-            </h1>
 
             <livewire:searchbox />
 
--- resources/views/livewire/v-card.blade.php
+++ resources/views/livewire/v-card.blade.php
@@ -1,7 +1,7 @@
 @vite('resources/css/app.css')
 
 <div class="relative bg-white max-w-xs	  text-black-600 p-4 rounded-tl-3xl shadow-lg flex flex-col items-center">
-    <div class="absolute top-0 right-0 bg-yellow-400 text-black text-sm font-semibold px-2 py-1 rounded-bl-lg">
+    <div class="absolute top-0 right-0 bg-yellow-400 text-white text-sm font-semibold px-2 py-1 rounded-bl-lg">
         Virtual Assistant
     </div>
     <img src="{{$data['photo']}}" alt="Jane P." class="w-24 bg-yellow-400 h-24 rounded-full mb-4">
@@ -19,8 +19,8 @@
         {{$data['description_intro']}}
     </p>
     <div class="flex flex-wrap justify-center space-x-2">
-        <span class="bg-yellow-400 text-black text-xs font-semibold px-2 py-1 m-1 rounded-full">#Efficiency</span>
-        <span class="bg-yellow-400 text-black text-xs font-semibold px-2 py-1 m-1 rounded-full">#Multitasking</span>
-        <span class="bg-yellow-400 text-black text-xs font-semibold px-2 py-1 m-1 rounded-full">#Admin</span>
+        <span class="bg-yellow-400 text-white text-xs font-semibold px-2 py-1 m-1 rounded-full">#Efficiency</span>
+        <span class="bg-yellow-400 text-white text-xs font-semibold px-2 py-1 m-1 rounded-full">#Multitasking</span>
+        <span class="bg-yellow-400 text-white text-xs font-semibold px-2 py-1 m-1 rounded-full">#Admin</span>
     </div>
 </div>
